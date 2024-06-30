d1958c17401ca26a975f892a877b336736e87f56
Naim Khan <naimkhan01856@gmail.com>
2024-06-30 10:43:34 +0600

    insert head done

---
+++ insert.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    Node *tail = b;
+    insert_at_head(head, 100);
+    
+    print(head);
+    return 0;
+}
